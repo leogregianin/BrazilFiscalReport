e8254f89b474f170a788e27044946489f948fcd0
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2026-03-24 13:18:58 -0300

    brazilfiscalreport: Organize the tests and fixtures

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,24 +10,24 @@ def runner():
 
 
 def test_generate_dacce(runner):
-    xml_path = "tests/fixtures/xml_cce_1.xml"
+    xml_path = "tests/fixtures/dacce/xml_cce_1.xml"
     result = runner.invoke(cli, ["dacce", xml_path])
     assert result.exit_code == 0, result.output
 
 
 def test_generate_danfe(runner):
-    xml_path = "tests/fixtures/nfe_test_1.xml"
+    xml_path = "tests/fixtures/danfe/nfe_test_1.xml"
     result = runner.invoke(cli, ["danfe", xml_path])
     assert result.exit_code == 0, result.output
 
 
 def test_generate_dacte(runner):
-    xml_path = "tests/fixtures/dacte_test_1.xml"
+    xml_path = "tests/fixtures/dacte/dacte_test_1.xml"
     result = runner.invoke(cli, ["dacte", xml_path])
     assert result.exit_code == 0, result.output
 
 
 def test_generate_damdfe(runner):
-    xml_path = "tests/fixtures/mdf-e_test_1.xml"
+    xml_path = "tests/fixtures/damdfe/mdf-e_test_1.xml"
     result = runner.invoke(cli, ["damdfe", xml_path])
     assert result.exit_code == 0, result.output
